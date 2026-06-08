ed1a9f90940eaae493281a8bced68fe9a4d9656a
Korivi Sukanya <korivisukanya02@gmail.com>
2026-06-08 18:35:37 +0530

    feat: add update contact feature

--- ContactBook.java
+++ ContactBook.java
@@ -64,7 +64,41 @@ public void listAllContacts(){
       }
 }
 
+public boolean checkContact(String name){
+        return contactlist.containsKey(name);
+   
+}
 
 
+public void updateContact(String name,Long number,String email){
+    
+  Contact obj1=new Contact(name,number,email);
+  contactlist.put(name,obj1);
+  System.out.println("Contact updated successfully!");  
 
+          }
+
+
+public void updateContact(String name,Long number){
+ 
+     Contact c=contactlist.get(name);
+     String email=c.email;
+    
+     Contact obj2=new Contact(name,number,email);
+     contactlist.put(name,obj2);
+     System.out.println("Contact updated successfully!");
+
+}
+
+
+public void updateContact(String name,String email){
+ 
+     Contact c=contactlist.get(name);
+     Long number=c.number;
+    
+     Contact obj3=new Contact(name,number,email);
+     contactlist.put(name,obj3);
+     System.out.println("Contact updated successfully!");
+
+}
 }
--- Main.java
+++ Main.java
@@ -22,7 +22,7 @@ boolean exit=false;
 
 while(!exit){
 
-System.out.println("Menu:\n1.Add Contact\n2.Search Contact\n3.delete Contact\n4.List All Contacts\n5.Exit\nEnter Your Choice");
+System.out.println("Menu:\n1.Add Contact\n2.Search Contact\n3.delete Contact\n4.List All Contacts\n5.Exit\n6.updateContact\nEnter Your Choice");
 
 int operation=sc. nextInt();
 sc.nextLine();
@@ -69,7 +69,50 @@ switch (operation){
      System.out.println("Goodbye!");
      exit=true;
      break;
+  case 6:
+     System.out.println("Enter Contact Name");
+     name=sc.nextLine();
 
+     if(cb.checkContact(name)){
+     System.out.println("What do you want  to update? Number/Email/Both");
+     String choice=sc.nextLine();
+    
+
+     if (choice.equals("Number")){
+
+          System.out.println("Enter Number");
+          number=sc.nextLong();
+          sc.nextLine();
+
+          cb.updateContact(name,number);}
+
+     else if(choice.equals("Email")){
+
+           System.out.println("Enter email");
+           email=sc.nextLine();
+ 
+           cb.updateContact(name,email);}
+
+      else{
+
+       System.out.println("Enter Number");
+          number=sc.nextLong();
+          sc.nextLine();
+
+
+           System.out.println("Enter email");
+           email=sc.nextLine();
+
+           cb.updateContact(name,number,email);}
+         
+
+     
+}
+else{
+           System.out.println("Contact not Found");}
+     
+break;
+     
 }
 
 }
